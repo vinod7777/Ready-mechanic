37f4740d61aca9674a3be0eee1702b22834edfc0
vinod7777 <sanapalavinodkumar37@gmail.com>
2025-09-05 23:35:16 +0530

    done

--- assets/js/page-loader.js
+++
@@ -1,7 +0,0 @@
-// Page Loader - Simple animation trigger
-document.addEventListener('DOMContentLoaded', function() {
-    // Add loaded class to body after a short delay
-    setTimeout(() => {
-        document.body.classList.add('loaded');
-    }, 100);
-});
